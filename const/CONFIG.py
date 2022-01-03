ac73bde537fa645eaff51cfe99a831d189c06599
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-03 14:35:53 +0530

    Update CONFIG.py

--- const/CONFIG.py
+++ const/CONFIG.py
@@ -1,6 +1,6 @@
 class CONFIG:
-    ADMINS = {1, 2}
-    BOTTOKEN = ""
+    ADMINS = {1511373882}
+    BOTTOKEN = "5006835603:AAHr54VEg3kq-epDtX66iw1Iyv8uWoTrwT0"
     IP_ADDR = ""
     PORT_NUM = 0
     # set PORT_NUM=0 for polling the updates instead of webhook
